f89824b6c52e082db027b572ef2dba52e0079ce9
Andi Muhammad Tamirn <andimuhammadtamrin@gamil.com>
2019-08-19 22:33:31 +0800

    Tugas 19 Java Script
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas19.js
@@ -0,0 +1,21 @@
+function perulangan(){
+  var ulang = "Semangat..."
+  console.log(ulang.repeat(5));
+
+}
+perulangan()
+console.log("");
+console.log("");
+function bintang(){
+  var test = "*"
+  console.log(test.repeat(5));
+  console.log(test.repeat(25));
+  console.log(test.repeat(45));
+  console.log(test.repeat(65));
+  console.log(test.repeat(95));
+  console.log(test.repeat(65));
+  console.log(test.repeat(45));
+  console.log(test.repeat(25));
+  console.log(test.repeat(5));
+}
+bintang()
